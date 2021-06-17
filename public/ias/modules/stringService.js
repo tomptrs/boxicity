168ec06b2088c22d136f44f60d254570443e1422
Matisse <faes.matisse@gmail.com>
2021-06-17 13:17:40 +0200

    index styling updated

---
+++ public/ias/modules/stringService.js
@@ -0,0 +1,15 @@
+export class StringIdentifier{
+    constructor(text, identifier){
+        this.text = text;
+        this.identifier = identifier;
+        
+    }
+}
+
+export const englishStrings = [];
+export const frenchStrings = [];
+export const dutchStrings = [];
+
+englishStrings.push('NEW LOBBY', 'newLobby');
+
+dutchStrings.push('NIEUWE LOBBY', 'newLobby');
